6f74d6af4714c72b55c06c1cd21806786bc7cd6b
Ashraf-Bahy <ashrafbahy286@gmail.com>
2023-09-10 20:35:49 +0300

    adding cvs to the user

--- resumeness/app.js
+++ resumeness/app.js
@@ -1,9 +1,20 @@
+// const multer = require('multer');
+// const upload = multer({ dest: 'uploads/' });
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const authRouter = require('./routes/auth.route.js');
- 
+const authRouter = require("./routes/auth.route.js");
+const authMiddleware = require("./middlewares/auth.middleware.js");
+const jwt = require("jsonwebtoken");
+const {
+    User,
+    signUpSchema,
+    logInSchema,
+    CoverLetter,
+    Resume,
+} = require("./models/User.js");
+
 dotenv.config();
 const app = express();
 
@@ -14,12 +25,54 @@ mongoose
     .catch((err) => {
         if (err) return console.error(err);
     });
-    
+
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/auth", authRouter);
+app.post("/addcv", async (req, res) => {
+  // Handle file upload logic here
+  console.log(req.body);
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+  const { cvFile } = req.body;
+
+  try {
+      // Verify the token and get the user's id
+      const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      // Get the user from the database
+      const user = await User.findById(id);
+      // Check if the user exists
+      if (!user) {
+          return res.status(404).json({ error: "User not found" });
+      }
+      const cv1 = new Resume({
+          RESUME: cvFile,
+      });
+      // Update the user's CV field with the file information
+      user.resumes.push(cv1);
+      // Save the updated user object
+      console.log(user);
+      await user.save();
+      console.log("Document saved successfully:", user);
+
+      // Return a success response
+      return res.status(200).json({ message: "CV uploaded successfully" });
+  } catch (err) {
+      console.log(err);
+      res.status(401).json({ error: "Invalid token" });
+  }
+});
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
--- resumeness/models/User.js
+++ resumeness/models/User.js
@@ -1,5 +1,22 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const model = mongoose.model;
 const Joi = require("joi");
+
+const resumeSchema = new Schema({
+    RESUME: {
+        type: mongoose.Schema.Types.Mixed,
+    }
+
+  });
+
+  // Define the schema for the user collection
+const CoverSchema = new Schema({
+    COVERLETTER: {
+        type: mongoose.Schema.Types.Mixed,
+    }
+  });
+
 const userSchema = new Schema({
     fullName: {
         type: String,
@@ -14,9 +31,16 @@ const userSchema = new Schema({
         type: String,
         required: true,
         select: false,
-    }
+    },
+
+    resumes: [resumeSchema],
+    coverLetters: [CoverSchema],
 });
+
 const User = model("User", userSchema);
+const CoverLetter = model("CoverLetter", resumeSchema);
+const Resume = model("Resume", CoverSchema);
+
 // Sign up validation schema
 const signUpSchema = Joi.object({
     fullName: Joi.string().min(3).max(25).required(),
@@ -28,4 +52,5 @@ const logInSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(8).max(32).required(),
 });
-module.exports = { User, signUpSchema, logInSchema };
+
+module.exports = { User, signUpSchema, logInSchema, CoverLetter, Resume };
