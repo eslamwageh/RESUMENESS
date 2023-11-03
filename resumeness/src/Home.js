c2b11d351939960519d40c82ddce5e7ce4858214
eslamwageh <53353517+eslamwageh@users.noreply.github.com>
2023-11-03 13:55:41 +0200

    done

--- resumeness/src/Home.js
+++ resumeness/src/Home.js
@@ -1,22 +1,31 @@
-import { Link } from 'react-router-dom';
-import Sign from './Sign';
+import { Link } from "react-router-dom";
+import Sign from "./Sign";
 const Home = () => {
     return (
         <div className="home">
-        <h1 className="home-title">RESUMENESS</h1>
-            <p className='home-paragraph'>Log in, fill in the blanks, and download your resume instantly.</p>
-        <div className="view">
-                <img className ='first' src="./photos/CV template2.jpg" alt="tmpimg" />
-                <img className ='second' src="photos/coverletter.webp" alt="tmpimg" />
-                <img className ='third' src="./photos/resume.jpg" alt="tmpimg" />
+            <h1 className="home-title">RESUMENESS</h1>
+            <p className="home-paragraph">
+                Log in, fill in the blanks, and download your resume instantly.
+            </p>
+            <div className="view">
+                <img
+                    className="first"
+                    src="./RESUMENESS/photos/CV template2.jpg"
+                    alt="tmpimg"
+                />
+                <img
+                    className="second"
+                    src="photos/coverletter.webp"
+                    alt="tmpimg"
+                />
+                <img className="third" src="./photos/resume.jpg" alt="tmpimg" />
             </div>
-            <Link to='/Login'>
+            <Link to="/Login">
                 <button className="but" type="submit">
                     Let's Start
-                </button>            
+                </button>
             </Link>
         </div>
-        
     );
-}
+};
 export default Home;
