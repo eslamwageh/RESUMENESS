e23cda6f9a21ad0f2bc8838166f618b6ead0598d
eslamwageh <53353517+eslamwageh@users.noreply.github.com>
2023-11-03 13:42:35 +0200

    done

--- resumeness/src/App.js
+++ resumeness/src/App.js
@@ -1,37 +1,63 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import Sign from './Sign';
-import Login from './Login';
-import Resume from './Resume';
-import About from './About';
-import Profile from './Profile';
-import Workdata from './Workdata';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Sign from "./Sign";
+import Login from "./Login";
+import Resume from "./Resume";
+import About from "./About";
+import Profile from "./Profile";
+import Workdata from "./Workdata";
 //import { library } from '@fortawesome/fontawesome-svg-core'
 
-import Preview from './Preview';
-import Coverletter from './Coverletter';
-import Coverletterpreview from './Coverletterpreview';
+import Preview from "./Preview";
+import Coverletter from "./Coverletter";
+import Coverletterpreview from "./Coverletterpreview";
 
 function App() {
-  return (
-    <Router>
-        <div className="App">
-        <Navbar/>
-          <Routes>
-            <Route exact path = "/" element = {<Home />}></Route>
-            <Route exact path = "/Login" element = {<Login />}></Route>
-            <Route exact path = "/Sign" element = {<Sign />}></Route>
-            <Route exact path = "/Profile" element = {<Profile />}></Route>
-            <Route exact path = "/Resume" element = {<Resume />}></Route>
-            <Route exact path = "/Coverletter" element = {<Coverletter />}></Route>
-            <Route path = "/Coverletter/Previewcoverletter" element = {<Coverletterpreview />}></Route>
-            <Route path = "/Resume/Preview" element = {<Preview />}></Route>
-            <Route path = "/About" element = {<About />}></Route> 
-          </Routes>
-        </div>
-    </Router> 
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route exact path="RESUMENESS/" element={<Home />}></Route>
+                    <Route
+                        exact
+                        path="RESUMENESS/Login"
+                        element={<Login />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="RESUMENESS/Sign"
+                        element={<Sign />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="RESUMENESS/Profile"
+                        element={<Profile />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="RESUMENESS/Resume"
+                        element={<Resume />}
+                    ></Route>
+                    <Route
+                        exact
+                        path="RESUMENESS/Coverletter"
+                        element={<Coverletter />}
+                    ></Route>
+                    <Route
+                        path="RESUMENESS/Coverletter/Previewcoverletter"
+                        element={<Coverletterpreview />}
+                    ></Route>
+                    <Route
+                        path="RESUMENESS/Resume/Preview"
+                        element={<Preview />}
+                    ></Route>
+                    <Route path="RESUMENESS/About" element={<About />}></Route>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 //hola
 export default App;
